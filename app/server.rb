29071390c290222b367a3ea3e9d30f90c888e34a
Fadie Hannona <fadiehannona@gmail.com>
2014-10-14 19:11:28 +0100

    Added DataMapper setup and branches

--- app/server.rb
+++ app/server.rb
@@ -1,11 +1,16 @@
 # pacific-atoll-5967 - LIVE
-# powerful-fjord-6331 -
+# powerful-fjord-6331 - STAGING
 
 env = ENV["RACK_ENV"] || "development"
 
 require 'sinatra'
 require 'data_mapper'
 require 'rack-protection'
+
+ENV["RACK_ENV"] == "production" ? DataMapper.setup(:default, ENV["DATABASE_URL"]) : DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+
+DataMapper.finalize # collect all models and work out what db is shaped like
+DataMapper.auto_upgrade! # will change db structure, if table exists do this if not then create this.
 
 get '/' do
 	"Hello World"
